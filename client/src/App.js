c2fcb683e9d903a2320d67585bc379a62678f131
Persistent <agrlarbig@gmail.com>
2023-04-03 20:27:26 -0600

    minor fixes and clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -37,11 +37,11 @@ function App() {
       <Router>
         <div>
           <Navbar />
-          <Switch>
+          <Routes>
             <Route exact path="/" component={SearchBooks} />
             <Route exact path="/saved" component={SavedBooks} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
+          </Routes>
         </div>
       </Router>
     </ApolloProvider>
